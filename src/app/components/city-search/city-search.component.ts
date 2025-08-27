24fb4349bb499172a12e6bf9ec89d7cdf20b64c2
lenapuletic <puleticlena@gmail.com>
2025-08-27 09:01:37 +0200

    feat: implement weather dashboard component

--- src/app/components/city-search/city-search.component.ts
+++ src/app/components/city-search/city-search.component.ts
@@ -25,48 +25,21 @@ export class CitySearchComponent {
 
   private weatherService = inject(WeatherService);
 
-
   searchControl = new FormControl('');
-  cities = ['New York', 'Los Angeles', 'London', 'Paris', 'Tokyo', 'Belgrade', 'Berlin']; // ToDo: add all cities available in the api
-  filteredCities: string[] = [];
   filteredCities$: Observable<GeolocationData[]>;
 
   constructor() {
     this.filteredCities$ = this.searchControl.valueChanges.pipe(
-      // Wait for 300ms after the user stops typing
-      debounceTime(300),
-      // Only proceed if the text has actually changed
-      distinctUntilChanged(),
-      // Only make an API call if the search term is 2+ characters long
-      filter(value => (value?.length ?? 0) >= 2),
-      // Switch to our new API call
       switchMap(value => this.weatherService.getCitySuggestions(value!))
     );
-    
-    // this.searchControl.valueChanges.subscribe(value => {
-    //   this.filteredCities = this.filterByInput(value || '');
-    // });
   }
 
   displayFn(city: GeolocationData): string {
-    console.log(city)
     return city && city.name ? `${city.name}, ${city.country}` : '';
   }
 
   onOptionSelected(event: MatAutocompleteSelectedEvent) {
     const city: GeolocationData = event.option.value;
     this.search.emit(city.name);
-  }
-
-  private filterByInput(value: string): string[] {
-    const filterValue = value.toLowerCase();
-    return this.cities.filter(city => city.toLowerCase().includes(filterValue));
-  }
-
-  onSearch() {
-    const city = this.searchControl.value?.trim();
-    if (city) {
-      this.search.emit(city);
-    }
   }
 }
--- src/app/components/weather-dashboard/weather-dashboard.component.ts
+++ src/app/components/weather-dashboard/weather-dashboard.component.ts
@@ -1,11 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, computed, inject } from '@angular/core';
+import { WeatherStore } from '../../store/weather.store';
+import { DatePipe, DecimalPipe } from '@angular/common';
 
 @Component({
   selector: 'app-weather-dashboard',
-  imports: [],
+  imports: [DatePipe, DecimalPipe],
   templateUrl: './weather-dashboard.component.html',
   styleUrl: './weather-dashboard.component.scss'
 })
 export class WeatherDashboardComponent {
+  readonly store = inject(WeatherStore);
 
+  readonly currentWeather = this.store.currentWeather;
+  readonly isLoading = this.store.isLoading;
+  readonly error = this.store.error;
+
+  readonly weatherIconUrl = computed(() => {
+    const iconCode = this.currentWeather()?.weather[0]?.icon;
+    return iconCode ? `https://openweathermap.org/img/wn/${iconCode}@2x.png` : '';
+  });
 }
