d4add1804d2739472af32b8284b471b676da9da8
lenapuletic <puleticlena@gmail.com>
2026-02-05 11:23:22 +0100

    fix: configure production api key and update readme image

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
     production: true,
     openWeather: {
-      key: 'PRODUCTION_API_KEY',
+      key: '27ab5d3586a67a9e7ff66343e6e67535',
       url: 'https://api.openweathermap.org/data/2.5'
     }
   };
